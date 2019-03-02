ea418e3ef15c7cfd476a8676815fbe25e380249e
TomyJPN <masaya120331@gmail.com>
2019-03-02 23:55:35 +0900

    スポナーを実装，執筆．UI画像を追加．

---
+++ shoot/Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+  public GameObject zombie;
+  [Range(1,2000)]
+  public int probability = 100;
+  void Update() {
+    if (Random.Range(1, probability) == 1) {
+      spawn();
+    }
+  }
+
+  void spawn() {
+    GameObject.Instantiate(zombie, this.transform.position, Quaternion.identity);
+  }
+}
